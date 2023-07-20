dac188479f34f35345164f8041588984b1051cfa
Gustavo Atencio Mauricio <gustavo.atencio.mauricio@outlook.com>
2023-07-20 12:54:18 -0500

    next config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ const { i18n } = require('./next-i18next.config');
 const nextConfig = {
   i18n,
   reactStrictMode: true,
+  swcMinify: true,
+  productionBrowserSourceMaps: false,
 
   webpack(config, { isServer, dev }) {
     config.experiments = {
